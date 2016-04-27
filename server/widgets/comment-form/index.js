358c13cf91aa8bdb45a52031f5db615e0920f312
albertosouza <alberto.souza.99@gmail.com>
2016-04-27 01:25:37 -0300

    add widget comments

---
+++ server/widgets/comment-form/index.js
@@ -0,0 +1,20 @@
+/**
+ * Widget comment-form main file
+ *
+ * See https://github.com/wejs/we-core/blob/master/lib/class/Widget.js for all Widget prototype functions
+ */
+
+module.exports = function (projectPath, Widget) {
+  var widget = new Widget('comment-form', __dirname);
+
+  widget.viewMiddleware = function viewMiddleware(widget, req, res, next) {
+    if (res.locals.model && res.locals.id) {
+      widget.haveRecord = true;
+    } else {
+      widget.hide = true;
+    }
+    return next();
+  }
+
+  return widget;
+};
